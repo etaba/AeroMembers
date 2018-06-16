f0e5e7aa7421bcdbd25ed9a7430c33782785ea65
etaba <eptaba@gmail.com>
2018-06-16 10:43:54 -0700

    nuke script tweaked for prod

--- AeroMembersApp/utilities/nuke.sh
+++ AeroMembersApp/utilities/nuke.sh
@@ -2,8 +2,11 @@
 
 mv ../migrations/loadItems.py .
 rm ../migrations/0*
-mysql -u plumber -pplumberpass -e 'drop database aeromembersdb'
-mysql -u plumber -pplumberpass -e 'create database aeromembersdb'
+if [ $PRODUCTION != true ]
+then
+	mysql -u plumber -pplumberpass -e 'drop database aeromembersdb'
+	mysql -u plumber -pplumberpass -e 'create database aeromembersdb'
+fi
 python ../../manage.py makemigrations
 mv loadItems.py ../migrations/
 python ../../manage.py migrate
